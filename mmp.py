ed5971aa48b2855623708a9661c67bb729010940
Darren Rambaud <dtrambaud@txstate.edu>
2017-04-03 06:00:43 -0500

    Implemented multi-threading, yay! Improved console output. It is slightly annoying but gives a psuedo-progress bar. Will update the repository with comments/documentation as well as giving appropriate credits to where it is due! (thanks /r/place for this fun learning project)

--- mmp.py
+++ mmp.py
@@ -4,8 +4,10 @@ import time
 import threading
 import random
 import requests
+
 from PIL import Image
 from requests.adapters import HTTPAdapter
+from threading import Barrier
 
 class Canvas:
     RGB_CODE_DICTIONARY = {
@@ -30,11 +32,13 @@ class Canvas:
     sessionObj = None
     loginObj = None
     percent = None
+    threadID = None
 
-    def __init__(self, sessionObj, loginObj, percent):
+    def __init__(self, sessionObj, loginObj, percent, threadID):
         self.sessionObj = sessionObj
         self.loginObj = loginObj
         self.percent = percent
+        self.threadID = threadID
 
     def distance(self, c1, c2):
         (r1, g1, b1) = c1
@@ -49,7 +53,7 @@ class Canvas:
         return code
 
     def place_pixel(self, ax, ay, new_color):
-        consoleMsg = "Probing absolute pixel ({},{})".format(ax, ay)
+        consoleMsg = "[{}] - Probing absolute pixel ({},{})".format(self.threadID, ax, ay)
 
         while True:
             self.loginObj = self.sessionObj.get("http://reddit.com/api/place/pixel.json?x={}&y={}".format(ax, ay),
@@ -67,23 +71,23 @@ class Canvas:
                 "user_name"] if "user_name" in data else "<nobody>"))
             time.sleep(.5)
         else:
-            print("{}: Placing color #{} at ({}, {})".format(consoleMsg, new_color, ax, ay))
+            print("{}: Placing color #{} ...".format(consoleMsg, new_color, ax, ay))
             self.loginObj = self.sessionObj.post("https://www.reddit.com/api/place/draw.json",
                                                  data={"x": str(ax), "y": str(ay), "color": str(new_color)})
 
             secs = float(self.loginObj.json()["wait_seconds"])
             if "error" not in self.loginObj.json():
-                consoleMsg = "Placed color, waiting {} seconds. {}% complete."
+                consoleMsg = "-[{}]-->Success! Placed color @({},{}), waiting {} seconds. {}% complete."
             else:
-                consoleMsg = "Cool down already active - waiting {} seconds. {}% complete."
+                consoleMsg = "-[{}]-->Cooldown is active for ({},{}), waiting {} seconds. {}% complete."
 
             timeToWait = int(secs) + 2
             while timeToWait > 0:
-                prog = consoleMsg.format(timeToWait, self.percent)
+                prog = consoleMsg.format(self.threadID, ax, ay, timeToWait, self.percent)
                 time.sleep(1)
                 timeToWait -= 1
                 if timeToWait > 0:
-                    print(prog, end="              \r")
+                    print(prog, end='           \r')
                 else:
                     print(prog)
 
@@ -127,18 +131,59 @@ class Session:
             except KeyError:
                 print("Bad login info, please verify the username/password combination "
                       "for \'{}\' and try again!".format(self.username))
-                self.username = input("Re-enter username: ")
-                self.password = input("Re-enter password: ")
+                self.username = input("Re-enter username (prev='{}'): ".format(self.username))
+                self.password = input("Re-enter password (prev='{}'): ".format(self.username))
             else:
                 break
 
 
+def distribute_pixel_placement(barrier, mutex, threadID, percentage, img, startPosition):
+    mutex.acquire() # allows user to input each username/pw without being interrupted by other threads
+    usr = input("[{}] Username: ".format(threadID + 1))
+    pwd = input("[{}] Password: ".format(threadID + 1))
+    mutex.release()
+    barrier.wait() # once user has entered the information, threads are released
+    while 1:
+        thrSession = Session(usr, pwd)
+        thrCanvas = Canvas(thrSession.session, thrSession.login, percentage, threadID)
+
+        print("Thread-{} :starting image placement for img height: {}, width: {}".format(threadID, img.height, img.width))
+        TwoDimArray = thrCanvas.shuffle2d([[[i,j] for i in range(img.width)] for j in range(img.height)])
+        total = img.width * img.height
+        checked = 0
+        for x in range(img.width):
+            for y in range(img.height):
+                xx = TwoDimArray[x][y]
+                pixel = img.getpixel((xx[0], xx[1]))
+
+                if pixel[3] > 0:
+                    pal = thrCanvas.find_palette((pixel[0], pixel[1], pixel[2]))
+
+                    ax = xx[0] + startPosition[0]
+                    ay = xx[1] + startPosition[1]
+
+                    thrCanvas.place_pixel(ax, ay, pal)
+                    checked += 1
+                    thrCanvas.percent = round((checked/total) * 100, 2)
+
+        consoleMessage = "All pixels placed, sleeping {}s..."
+        timeToWait = 60
+        while timeToWait > 0:
+            msg = consoleMessage.format(timeToWait)
+            time.sleep(1)
+            timeToWait -= 1
+            if timeToWait > 0:
+                print(msg, end='              \r')
+            else:
+                print(msg)
+
+
 def main():
     # read from command line arguments
-    #img = Image.open(sys.argv[1]) # open up desired image
-    #origin = (int(sys.argv[2]), int(sys.argv[3])) # position of top left image (x and y on canvas)
-    img = Image.open("test.png")  # todo: add check for if file exists
-    origin = (808, 641) # todo: get rid of hard code
+    #targetImage = Image.open(sys.argv[1]) # open up desired image
+    #start = (int(sys.argv[2]), int(sys.argv[3])) # position of top left image (x and y on canvas)
+    targetImage = Image.open("test.png")  # todo: add check for if file exists
+    start = (808, 641) # todo: get rid of hard code
     percentage = 0
 
     # can reprocess this so that it creates a thread for each valid username and password combination ...
@@ -155,48 +200,13 @@ def main():
         except ValueError as err:
             print(err.args)
 
+    mutex = threading.Lock()
+    barrier = Barrier(int(numberOfAccounts), timeout=50)
     for account in range(0, int(numberOfAccounts)):
-        #     #for each account query user for username and password for reddit
-        usr = input("Enter your username for reddit: ")
-        passwrd = input("Enter your password: ")
-
-        # for each username and password, create a new thread for each and test each one
-        # create new thread here???
-        #     #create a new thread
-        #     #establish a valid session
-        #     #login/...
-        while 1:
-            thrSession = Session(usr, passwrd)
-            thrCanvas = Canvas(thrSession.session, thrSession.login, percentage)
-
-            print("starting image placement for img height: {}, width: {}".format(img.height, img.width))
-            TwoDimArray = thrCanvas.shuffle2d([[[i,j] for i in range(img.width)] for j in range(img.height)])
-            total = img.width * img.height
-            checked = 0
-            for x in range(img.width):
-                for y in range(img.height):
-                    xx = TwoDimArray[x][y]
-                    pixel = img.getpixel((xx[0], xx[1]))
-
-                    if pixel[3] > 0:
-                        pal = thrCanvas.find_palette((pixel[0], pixel[1], pixel[2]))
-
-                        ax = xx[0] + origin[0]
-                        ay = xx[1] + origin[1]
-
-                        thrCanvas.place_pixel(ax, ay, pal)
-                        checked += 1
-                        thrCanvas.percent = round((checked/total) * 100, 2)
-            consoleMessage = "All pixels placed, sleeping {}s..."
-            timeToWait = 60
-            while timeToWait > 0:
-                msg = consoleMessage.format(timeToWait)
-                time.sleep(1)
-                timeToWait -= 1
-                if timeToWait > 0:
-                    print(msg, end="              \r")
-                else:
-                    print(msg)
+        new_thread = threading.Thread(target=distribute_pixel_placement,
+                                      args=(barrier, mutex, account, percentage, targetImage, start,))
+        # new_thread.daemon = True
+        new_thread.start()
 
 if __name__ == "__main__":
     main()
